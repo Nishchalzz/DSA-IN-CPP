934557fbe6dfe87f442978bb442a16ef6513073e
Nishchal K J <nishchalgowda02@gmail.com>
2026-03-04 18:30:45 +0530

    longest subsequence rec mem tab

--- DAY47/longest_subsequence_rec.cpp
+++ DAY47/longest_subsequence_rec.cpp
@@ -52,6 +52,25 @@ int lsub_mem(string s1, string s2,vector<vector<int>>& dp){
 }
 
 
+int lcsTab(string s1, string s2){
+    int n = s1.size();
+    int m = s2.size();
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(s1[i-1]==s2[j-1]){
+                dp[i][j] = 1 + dp[i-1][j-1];
+            }else{
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+    }
+
+    return dp[n][m];
+}
+
+
 int main(){
 
     string s1 = "abcdge";
@@ -62,6 +81,8 @@ int main(){
     // cout<<lsub_rec(s1,s2);
 
     cout<<lsub_mem(s1,s2, dp);
+
+    cout<<lcsTab(s1,s2);
 
 
     return 0;
