5b25b44f9541bfa03b486606555ba004b30a19e0
Nishchal K J <nishchalgowda02@gmail.com>
2026-04-05 17:50:57 +0530

    peak ele

---
+++ Binary-search/peak_element.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& arr) {
+        int n = arr.size();
+        if(n==1) return 0;
+        if(arr[0]>arr[1]) return 0;
+        if(arr[n-1]>arr[n-2]) return n-1;
+
+        int low = 1, high = n-2;
+        while(low<=high){
+            int mid=(low+high)/2;
+
+            if(arr[mid] > arr[mid+1] && arr[mid]>arr[mid-1])
+                return mid;
+
+            else if(arr[mid]>arr[mid-1])      //slope is up so move forward by  low
+                    low = mid+1;
+            else                              //for mulitple point it can have a point in btw so have else here 
+                high = mid - 1;
+        }
+
+        return -1;
+
+    }
+};
