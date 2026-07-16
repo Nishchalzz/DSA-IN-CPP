1499ff2a55e03af44c7f341204844c1db8f7fc26
Nishchal02 <144874495+Nishchalzz@users.noreply.github.com>
2026-07-16 10:15:26 +0530

    Time: 20 ms (47.97%), Space: 46.6 MB (51.09%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,62 @@
+class disjointset{
+private:
+    vector<int> rank,par;
+public:
+    disjointset(int n){
+        rank.resize(n,0);
+        par.resize(n);
+        for(int i=0;i<n;i++)
+            par[i] = i;
+    }
+
+    int findpar(int node){
+        if(node == par[node])
+            return node;
+        return par[node] = findpar(par[node]);
+    }
+
+    void unionbyrank(int u, int v){
+        int ult_par_u = findpar(u);
+        int ult_par_v = findpar(v);
+
+        if(ult_par_u == ult_par_v) return;
+
+        if(rank[ult_par_u] > rank[ult_par_v])
+            par[ult_par_v] = ult_par_u;
+        else if(rank[ult_par_v] > rank[ult_par_u])
+            par[ult_par_u] = ult_par_v;
+        else if(rank[ult_par_v] == rank[ult_par_u]){
+            par[ult_par_u] = ult_par_v;
+            rank[ult_par_v]++;
+        }
+
+    }
+};
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        disjointset ds(n);
+        int countExtra = 0;
+        int countConnection = 0;
+        int totalEdge = connections.size();
+        int components = n;
+        for(auto it:connections){
+            int u = it[0];
+            int v = it[1];
+            int parU = ds.findpar(u);
+            int parV = ds.findpar(v);
+
+            if(parU == parV) countExtra++;
+            else{
+                ds.unionbyrank(u,v);
+                countConnection++;
+                components--;
+            }
+        }
+        //extra edges is count extra , count connection == nodes+1
+       if(totalEdge<n-1)
+            return -1;
+
+        return components-1;
+    }
+};
