ffc651206f10d7334ae8cae358d7223258fcef40
Nishchal02 <144874495+Nishchalzz@users.noreply.github.com>
2026-07-20 12:06:29 +0530

    Time: 0 ms (100%), Space: 8.6 MB (37.99%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int helper(int n,vector<int>& dp){
-
+        //memoization
        if(n==0 || n==1) return 1;
 
        if(dp[n]!=-1) return dp[n];
@@ -16,8 +16,13 @@ public:
         vector<int> dp(n+1,-1);
         dp[0] = 1;
         dp[1] = 1;
+        if(n<=1) return dp[n];
+        //tabulation
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
 
-        helper(n,dp);
+        // helper(n,dp);
 
         return dp[n];
     }
