3dee87df6a71d882479977033a10e679d37e518b
Nishchal02 <144874495+Nishchalzz@users.noreply.github.com>
2026-07-20 12:09:14 +0530

    Time: 0 ms (100%), Space: 7.8 MB (88.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -13,17 +13,18 @@ public:
     }
 
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        dp[0] = 1;
-        dp[1] = 1;
-        if(n<=1) return dp[n];
+        
+        if(n<=1) return 1;
+        int prev = 1; int prev2 = 1;
         //tabulation
         for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2];
+           int curr = prev+prev2;
+           prev2 = prev;
+           prev = curr;     
         }
 
         // helper(n,dp);
 
-        return dp[n];
+        return prev;
     }
 };
