3528a7dc1d75cc5d767a93bc07c2d6649a19bf3e
Nishchal02 <144874495+Nishchalzz@users.noreply.github.com>
2026-07-22 09:42:47 +0530

    Time: 0 ms (100%), Space: 9.5 MB (30.65%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -14,6 +14,19 @@ public:
 
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        return helper(m-1,n-1,dp);
+        dp[0][0] = 1;
+        
+        for(int r=0;r<m;r++){
+            for(int c=0;c<n;c++){
+                if(r==0 && c==0) continue;
+                dp[r][c] = 0;
+                if(r-1>=0)
+                    dp[r][c] = dp[r-1][c];
+                if(c-1>=0)
+                    dp[r][c] +=  dp[r][c-1];
+            }
+        }
+
+        return dp[m-1][n-1];
     }
 };
