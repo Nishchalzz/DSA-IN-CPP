ee36bb332d6830652a9492ad735e647cbcc29863
Nishchal02 <144874495+Nishchalzz@users.noreply.github.com>
2026-07-27 16:37:51 +0530

    Time: 125 ms (60.69%), Space: 13.7 MB (80.32%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -33,25 +33,24 @@ public:
             total+=nums[i];
         }
         if(total%2!=0) return false;
-        vector<vector<bool>> dp(n,vector<bool>(total/2+1, false));
+        vector<bool> dp(total/2+1,false),curr(total/2+1,false);
 
-        for(int i=0;i<n;i++)
-            dp[i][0]=true;
-        
-        if(nums[0] <= total/2)
-            dp[0][nums[0]] = true;
+        dp[0]=true;
+        curr[0] = true;
+        if(nums[0]<total/2+1)
+            dp[nums[0]] = true;
 
         for(int i=1;i<n;i++){
             for(int target = 1;target<total/2+1;target++){
                 bool pick = false;
                 if(target-nums[i]>=0)
-                    pick = dp[i-1][target-nums[i]];
-                bool notpick = dp[i-1][target];
+                    pick = dp[target-nums[i]];
+                bool notpick = dp[target];
 
-                dp[i][target] = pick || notpick;
-
+                curr[target] = pick || notpick;
             }
+            dp = curr;
         }
-        return dp[n-1][total/2];
+        return curr[total/2];
     }
 };
